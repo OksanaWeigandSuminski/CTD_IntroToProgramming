c67f52130d4678f6041e182dca02e446aee807d0
OksanaWeigandSu <>
2021-04-08 01:04:37 +0000

    lesson2 assignment

---
+++ Javascript-Functions/index.js
@@ -0,0 +1,67 @@
+// You need to fork this repl.it and then will turn in the link in your browswer as yoru homework assignment once you are done answering the following questions.
+
+//TESTING you should be able to test your answers by clicking into Shell on the right and typing 'npm test'.
+
+//DEBUGGING type console.log('any string here', varName) to console log values to see your answers before you run test.
+
+
+// QUESTION 1. Write a standard function definition that takes 1 parameter of data type string and prints out the parameter to the console. The parameter should have the value of "Happy Birthday!" You are given the beginning of the function.
+
+function happyBirthday() {
+
+}
+
+// QUESTION 2. Write a standard function definition that that takes 1 parameter of data type string and prints out the parameter to the console. The parameter should be of data type date. 
+
+//complete the today function below this line
+function today() {
+
+}
+
+
+// QUESTION 3. Write a function definition that takes 2 parameters, that are of data type string, and returns a new string that is a concatenation of the first parameter with the second parameter. (EX: parameter = 'Hello', returnValue='Hello there!'). You are given the beginning of the arrow function to complete. 
+
+const newString = () => {
+}
+
+// QUESTION 4. Write a function definition that takes 1 parameter, that is of data type number, and returns the factorial of that number. Complete the "factorial" function below
+
+const factorial = () => {
+
+}
+
+// QUESTION 5. Write a function definition that takes 2 parameters of data type number, and returns the modulus of parameter 1 by parameter 2. 
+
+//complete mod function below this line
+
+const mod = () => {
+
+}
+
+
+// QUESTION 6. Write a function definition that takes 2 parameters of data type number. The function will return the avergage of the 2 numbers. This time you need to write the "avg" function from scratch below
+
+//complete avg function below this line
+const avg = () => {
+
+}
+
+
+// QUESTION 7. Write a function definition that takes 2 parameters of data type integer/number. The function will check to see if the first parameter is smaller, bigger or equal to the second parameter. If the first parameter is smaller than the second, the first parameter will be subtracted by the second. If the first parameter is bigger than the second, the second parameter will be subtracted by the first. If they are both equal, return the first parameter subtracted by the second. Create 'checkNums' function below.
+
+//code here
+const checkNums = () => {
+
+}
+
+// QUESTION 8. Write a function definition that takes 2 parameters of data type string. The function will check to see if the first parameter is shorter, longer or equal in length to the second parameter. If the first parameter is shorter than the second, then return the length of the shorter string. If the first parameter is longer than the second, return the length of the longest string. If they are both equal, return the length of the string. 
+
+//code here
+const checkStringLength = () => {
+
+}
+
+
+module.exports = {
+  happyBirthday, today, newString, factorial, mod, avg, checkNums, checkStringLength
+}
