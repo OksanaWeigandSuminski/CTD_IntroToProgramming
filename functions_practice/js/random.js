25cedb853d531505e94c0bc1317f69084c2b5024
Oksana Weigand-Suminski <oxanapiven@gmail.com>
2021-04-13 21:32:32 -0400

    treeHouse functions js tutorial practice

---
+++ functions_practice/js/random.js
@@ -0,0 +1,8 @@
+function getRandomNumber() {
+  const randomNumber = Math.floor( Math.random() * 6 ) + 1;
+ return randomNumber;
+}
+
+alert (getRandomNumber());
+console.log(getRandomNumber());
+const dieRoll = getRandomNumber();
